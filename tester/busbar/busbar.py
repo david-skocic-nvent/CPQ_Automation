32688ca5daab23f3af2a8a5f699f834c88b61d0b
david-skocic-nvent <david.skocic@nvent.com>
2024-06-27 08:40:39 -0400

    multithreaded and ran most tests for busbar

--- tester/busbar/busbar.py
+++ tester/busbar/busbar.py
@@ -16,10 +16,12 @@ FIELD_NAMES = ["part number", "configuration", "bar thickness", "bar width", "ba
 LINK = "https://nventefs-admin.tactoncpq.com/!tickets%7ET-00000938/solution/list"
 HTML_DUMP_FILE_PATH = "C:\\Users\\E2023355\\OneDrive - nVent Management Company\\Documents\\VSCode\\Projects\\CPQ\\CPQ_Automation\\dump.html"
 
-drivers = []
+drivers: List[webdriver.Edge] = []
 threads: List[threading.Thread] = []
 drivers_in_use = []
-NUMBER_OF_THREADS = 2
+NUMBER_OF_THREADS = 1
+ALL_TEST_VALUES_FILE_PATH = "C:\\Users\\E2023355\\OneDrive - nVent Management Company\\Documents\\VSCode\\Projects\\CPQ\\CPQ_Automation\\tester\\busbar\\parsed_numbers.csv"
+COMPLETED_PARTS_FILE_PATH = "C:\\Users\\E2023355\\OneDrive - nVent Management Company\\Documents\\VSCode\\Projects\\CPQ\\CPQ_Automation\\tester\\busbar\\part_prices.csv"
 
 def make_drivers():
     global drivers
@@ -35,13 +37,21 @@ def login (driver):
     # clicks the login button
     click_element(driver, (By.XPATH, "//button"))
 
-def read_csv ():
-    csv_file = open("C:\\Users\\E2023355\\OneDrive - nVent Management Company\\Documents\\VSCode\\Projects\\CPQ\\CPQ_Automation\\tester\\busbar\\parsed_numbers.csv", "r", newline='')
+def read_csv_dict_list (file_path):
+    csv_file = open(file_path, "r", newline='')
     reader = csv.DictReader(csv_file)
     dict_list = []
     for row in reader:
         dict_list.append(row)
     return dict_list
+
+def read_csv_list (file_path):
+    csv_file = open(file_path, "r", newline='')
+    reader = csv.reader(csv_file)
+    return_list = []
+    for row in reader:
+        return_list.append(row)
+    return return_list
 
 def write_csv(row):
     csv_file = open("C:\\Users\\E2023355\\OneDrive - nVent Management Company\\Documents\\VSCode\\Projects\\CPQ\\CPQ_Automation\\tester\\busbar\\part_prices.csv", "a", newline='')
@@ -70,36 +80,42 @@ def get_out_of_selection (driver: webdriver.Edge):
     body.send_keys(Keys.TAB)
 
 
-def fill_values(driver, values):
+def run_test(driver, values):
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-46e978dfe6-a6a6-4bbf-8d0e-dd802ba7fbac"]'), [values["configuration"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
 
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-46440960ce-4964-4b1d-a59e-df51da1eb862"]'), [values["bar thickness"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
 
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-460d760aa7-b2b7-4669-baad-b75e692488e6"]'), [values["bar width"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
 
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-46da4742ce-7c9d-4611-96c9-cf3cdc755ba1"]'), [values["bar length"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
 
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-4603b44eaf-d4e6-4ae2-a9e6-e80e6078a05f"]'), [values["hole pattern"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
 
     choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-46b473b4b3-47bb-425a-a88e-c844d5983042"]'), [values["hole size"]])
     get_out_of_selection(driver)
-    time.sleep(2)
+    time.sleep(1)
     try_to_exit_warning (driver)
+
+    choose_textbox_value(driver, (By.XPATH, '//*[@id="typeahead-widget-null-8cbc05ee-34f6-43f9-a1d0-af9a771b7ac0-46028e1b03-2f59-4b5d-ab93-a5fe5903969c"]'), ["a\b" + values["material"]])
+    time.sleep(0.5)
+    get_out_of_selection(driver)
+    time.sleep(1)
+    try_to_exit_warning(driver)
 
     read_prices(driver, values)
 
@@ -114,11 +130,41 @@ def read_prices (driver, values):
         return_list.append(read_value(driver, (By.XPATH, f'//*[@id="bom-item-{i}"]/td[5]/span/span')))
         return_list.append(read_value(driver, (By.XPATH, f'//*[@id="bom-item-{i}"]/td[6]/span/span')))
 
-    write_csv(return_list)
+    # a last check to see if the part number is put in correctly
+
+    if return_list[0] != read_value (driver, (By.XPATH, '//*[@id="widget-null-90a80895-c989-4fe3-8d3e-8f141408f9b9-4616533bf9-3595-470c-9006-a4615b24d6dd-row"]/div[2]/div/div/input')):
+        print (f"Part number {[return_list[0]]} was entered incorrectly.")
+    else:
+        write_csv(return_list)
 
 def main():
     global drivers
-    tests = read_csv()
+    tests = read_csv_dict_list(ALL_TEST_VALUES_FILE_PATH)
+    completed_tests = read_csv_list(COMPLETED_PARTS_FILE_PATH)
+
+    # get a list of the completed part numbers
+    completed_part_numbers = []
+    for row in completed_tests:
+        completed_part_numbers.append(row[0])
+
+    print(len(completed_part_numbers))
+    print(len(tests))
+
+    tests_to_remove = []
+
+    # find all completed tests in tests list and add them to a remove list
+    for test in tests:
+        if test["part number"] in completed_part_numbers:
+            completed_part_numbers.remove(test["part number"])
+            tests_to_remove.append(test)
+
+    # remove all completed tests from tests
+    for test in tests_to_remove:
+        tests.remove(test)
+
+    for test in tests:
+        if test["hole size"] != "other":
+            print(test["part number"])
 
     make_drivers()
 
@@ -130,19 +176,16 @@ def main():
     for i in range(NUMBER_OF_THREADS):
         threads[i].join()
 
-    i = 1
-
     while len(tests) > 0:
-        if tests[0]["hole size"] != "other":
-            for i, thread in enumerate(threads):
-                if not thread.is_alive():
-                    threads[i] = threading.Thread(target=fill_values, args=(drivers[i],tests[0]))
-                    threads[i].start()
-                    print (f"Started running test {i}")
-                    i = i + 1
+        for i, thread in enumerate(threads):
+            if not thread.is_alive():
+                while tests[0]["hole size"] == "other":
                     tests.pop(0)
-        else:
-            tests.pop(0)
+                threads[i] = threading.Thread(target=run_test, args=(drivers[i],tests[0]))
+                threads[i].start()
+                print (f"Started running test {i}")
+                i = i + 1
+                tests.pop(0)
         time.sleep(2)
 
     time.sleep(5)
